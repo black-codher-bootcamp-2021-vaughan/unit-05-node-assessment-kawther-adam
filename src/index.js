4c42186b2f33856619b2e525672386611879f6ae
Kawther <kawtheradam63@gmail.com>
2022-04-04 13:42:20 +0100

    Node

--- src/index.js
+++ src/index.js
@@ -19,25 +19,68 @@ app.use(bodyParser.json());
 app.use("/content", express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
-  /*
+  
   res.sendFile("./public/index.html", { root: __dirname });
-  */
-  res.status(501).end();
+  
+  // res.status(501).end();
 });
 
 app.get('/todos', (_, res) => {
-  /*
+  
   res.header("Content-Type","application/json");
   res.sendFile(todoFilePath, { root: __dirname });
-  */
-  res.status(501).end();
+  
+  // res.status(501).end();
 });
 
 //Add GET request with path '/todos/overdue'
+app.get('/todos/overdue', (_, res) => {
+  res.header("Content-Type","application/json");
+  
+    const todo = todos.filter( todo => {
+       let nowDate = new Date();
+       let toDoDate =  new Date( todo.due )
+       if ( toDoDate <= nowDate ){
+         return true;
+       }
+    } );
+    res.write( JSON.stringify(todo) );
+    res.status(200).end();
+  });
+
 
 //Add GET request with path '/todos/completed'
+app.get('/todos/completed', (_, res) => {
+  res.header("Content-Type","application/json");
+  
+    const todo = todos.filter( todo => {
+       if ( todo.completed ){
+         return true;
+       }
+    } );
+    res.json(todo);
+    res.write( JSON.stringify(todo) );
+    res.status(200).end();
+  });
+
 
 //Add POST request with path '/todos'
+app.post('/todos', (req, res) => {
+  var fs = require("fs");
+  console.log( req.body );
+  todos.push( ( req.body ) );
+  if (!todos) return res.sendStatus(400);
+  fs.writeFile( __dirname + todoFilePath , JSON.stringify(todos) , err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+});
+  res.json(todos);
+
+ });
+
+
 
 //Add PATCH request with path '/todos/:id
 
